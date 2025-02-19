a133347d25b9c0b8105292f3cbeb96339a368f04
Moaz Samir <mezomezo1700@gmail.com>
2025-02-19 17:22:44 +0200

    Add files via upload

---
+++ scribt.js
@@ -0,0 +1,10 @@
+// document.querySelector(".menu").addEventListener("click", ()=>
+// { console.querySelector(".nav-itam").classList.toggel("active");
+
+// })
+
+// // document.querySelectorAll(".nav-link").forEach((item) => {
+// //   item.addEventListener("click", () => {
+// //     document.querySelector(".nav-item").classList.toggle("active");
+// //   });
+// // });
